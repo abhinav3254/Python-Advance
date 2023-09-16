29e3048001b68c343374a1cc22f891b9682f1c50
abhinav3254 <abhinav3254@gmail.com>
2023-09-16 22:54:11 +0530

    read csv and json

--- panda/one.py
+++ panda/one.py
@@ -6,3 +6,18 @@
 # myvar = pandas.DataFrame(mydataset)
 
 # print(myvar)
+
+# reading csv files
+
+import pandas as pd
+
+# note this --> point If you have a large DataFrame with many rows, Pandas will only return the first 5 rows, and the last 5 rows:
+# df = pd.read_csv(
+#     "/Users/abhinavkumar/Desktop/datasets/archive/zudio/Men/Men/ACCESSORIES.csv"
+# )
+# print(df.to_string)
+
+
+# reading json
+df = pd.read_json("/Users/abhinavkumar/Desktop/datasets/amazon.books.json", lines=True)
+print(df.to_string)
