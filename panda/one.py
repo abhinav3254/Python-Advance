2d3351b5e51caf06d8c11328b9a352f1bed8ff1b
abhinav3254 <abhinav3254@gmail.com>
2023-09-16 22:47:10 +0530

    dataframe

---
+++ panda/one.py
@@ -0,0 +1,8 @@
+# import pandas
+
+# mydataset = {"cars": ["BMW", "Volvo", "Ford"], "passings": [3, 7, 2]}
+
+# # dataframe is 2d array with columns of potentially different types
+# myvar = pandas.DataFrame(mydataset)
+
+# print(myvar)
